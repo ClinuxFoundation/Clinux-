bb8c428639b9234471344653199ba43c689d470e
Davi <languith54@gmail.com>
2026-06-05 11:04:58 -0300

    update clinux/__init__.py

--- clinux/__init__.py
+++ clinux/__init__.py
@@ -1,12 +1,4 @@
-"""
-CLINUX - Gerenciador de ambientes Linux com QEMU e PRoot.
-"""
+"""CLINUX - Gerenciador de ambientes Linux com QEMU e PRoot."""
 
 __version__ = "1.0.0"
-__author__ = "CLINUX Team"
-
-from .storage import Storage
-from .qemu import QEMUManager
-from .proot import PRootManager
-
-__all__ = ["Storage", "QEMUManager", "PRootManager"]
+__all__ = []
